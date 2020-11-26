f6619498e516eec91db5da8bb4d46958da0d0b4c
Michael Lee <mikecovlee@163.com>
2020-11-26 18:09:16 +0800

    Update curl.cpp

--- curl.cpp
+++ curl.cpp
@@ -66,10 +66,31 @@ CNI_ROOT_NAMESPACE {
 
 	CNI(make_session_ios)
 
+	CNI_NAMESPACE(ssl_level)
+	{
+		CNI_VALUE(none,    curl::ssl_level::none)
+		CNI_VALUE(try_use, curl::ssl_level::try_use)
+		CNI_VALUE(control, curl::ssl_level::control)
+		CNI_VALUE(all,     curl::ssl_level::all)
+	}
+
 	CNI_NAMESPACE(session)
 	{
-		CNI_V(set_url, &curl::session::set_url);
-		CNI_V(perform, &curl::session::perform);
+		CNI_V(set_url,                 &curl::session::set_url)
+		CNI_V(set_tcp_keep_alive,      &curl::session::set_tcp_keep_alive)
+		CNI_V(set_http_post,           &curl::session::set_http_post)
+		CNI_V(use_ssl,                 &curl::session::use_ssl)
+		CNI_V(set_ssl_verify_host,     &curl::session::set_ssl_verify_host)
+		CNI_V(set_ssl_verify_peer,     &curl::session::set_ssl_verify_peer)
+		CNI_V(set_ssl_cert,            &curl::session::set_ssl_cert)
+		CNI_V(set_ssl_key,             &curl::session::set_ssl_key)
+		CNI_V(set_ssl_passwd,          &curl::session::set_ssl_passwd)
+		CNI_V(set_connect_timeout,     &curl::session::set_connect_timeout)
+		CNI_V(set_connect_timeout_ms,  &curl::session::set_connect_timeout_ms)
+		CNI_V(set_accept_timeout_ms,   &curl::session::set_accept_timeout_ms)
+		CNI_V(set_transmit_timeout,    &curl::session::set_transmit_timeout)
+		CNI_V(set_transmit_timeout_ms, &curl::session::set_transmit_timeout_ms)
+		CNI_V(perform,                 &curl::session::perform)
 	}
 
 	CNI_TYPE_EXT(buffer, buffer_t, std::make_shared<std::stringstream>())
